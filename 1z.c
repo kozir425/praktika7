2a8386a52aec8c683a1590779346e94b7b194e6f
kozir425 <maksonchikhasanshin@gmail.com>
2025-04-28 08:52:53 +0300

    1z.c

---
+++ 1z.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Функция для нахождения индекса слова в строке
+int indexOf(char text[], char word[]) {
+    int textLength = 0;
+    int wordLength = 0;
+
+    // Считаем длину text
+    while (text[textLength] != '\0') {
+        textLength++;
+    }
+
+    // Считаем длину word
+    while (word[wordLength] != '\0') {
+        wordLength++;
+    }
+
+    // Ищем word в text
+    for (int i = 0; i <= textLength - wordLength; i++) {
+        int match = 1;
+        for (int j = 0; j < wordLength; j++) {
+            if (text[i + j] != word[j]) {
+                match = 0;
+                break;
+            }
+        }
+        if (match == 1) {
+            return i; // Возвращаем индекс, если нашли
+        }
+    }
+
+    return -1; // Если не нашли, возвращаем -1
+}
+
+  
